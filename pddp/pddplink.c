fb4e720d6aa2ab154652cc8ac2e1ef0b1b12ac6a
Hans-Christoph Steiner <eighthave@users.sourceforge.net>
2007-06-13 04:17:19 +0000

    made links blue, but a different blue than Pd selected text, plus hover turns red
    svn path=/trunk/externals/miXed/; revision=7770

--- pddp/pddplink.c
+++ pddp/pddplink.c
@@ -98,7 +98,7 @@ static void pddplink_select(t_gobj *z, t_glist *glist, int state)
 	    sys_vgui(".x%lx.c itemconfigure %s -fill blue\n",
 		     glist, rtext_gettag(y));
 	else
-	    sys_vgui(".x%lx.c itemconfigure %s -text {%s} -fill magenta\n",
+	    sys_vgui(".x%lx.c itemconfigure %s -text {%s} -fill #0000dd -activefill #e70000\n",
 		     glist, rtext_gettag(y), x->x_vistext);
     }
 }
@@ -121,7 +121,7 @@ static void pddplink_vis(t_gobj *z, t_glist *glist, int vis)
             && (y = glist_findrtext(glist, (t_text *)x)))
         {
             rtext_draw(y);
-	    sys_vgui(".x%lx.c itemconfigure %s -text {%s} -fill magenta\n",
+	    sys_vgui(".x%lx.c itemconfigure %s -text {%s} -fill #0000dd -activefill #e70000\n",
 		     glist_getcanvas(glist), rtext_gettag(y), x->x_vistext);
         }
     }
