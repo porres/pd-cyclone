dff83de02c3d7e7a4ef00a55b128d51ca6b17cbf
derekxkwan <derek.x.kwan@gmail.com>
2016-04-02 03:42:43 -0700

    adding updating authorship to matrix~

--- sickle/matrix.c
+++ sickle/matrix.c
@@ -1,6 +1,7 @@
 /* Copyright (c) 2005 krzYszcz and others.
  * For information on usage and redistribution, and for a DISCLAIMER OF ALL
  * WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
+//updating 2016 - Derek Kwan
 
 #include "m_pd.h"
 #include "common/loud.h"
