c59953e4a2eb74568539869c75f74e00279c6979
porres <porres@gmail.com>
2016-04-04 15:44:03 -0300

    seemingly fixed thresh code

--- sickle/thresh.c
+++ sickle/thresh.c
@@ -1,67 +1,64 @@
 /* Copyright (c) 2016 Porres
- * For information on usage and redistribution, and for a DISCLAIMER OF ALL
- * WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
+* For information on usage and redistribution, and for a DISCLAIMER OF ALL
+* WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
 
 #include "m_pd.h"
+#include "shared.h"
 #include "sickle/sic.h"
 
 #define THRESH_DEFLO  0.
 #define THRESH_DEFHI  0.
 
-/*typedef t_sic t_thresh;*/
-
 typedef struct _thresh
 {
     t_sic    x_sic;
-    t_float  x_lastout;
+    t_float  x_last;
 } t_thresh;
-
 
 static t_class *thresh_class;
 
 static t_int *thresh_perform(t_int *w)
 {
-    int nblock = (int)(w[1]);
-    t_float *in1 = (t_float *)(w[2]);
-    t_float *in2 = (t_float *)(w[3]);
-    t_float *in3 = (t_float *)(w[4]);
-    t_float *out = (t_float *)(w[5]);
-    t_float lastout = x->x_lastout;
+    t_thresh *x = (t_thresh *)(w[1]);
+    int nblock = (int)(w[2]);
+    t_float *in1 = (t_float *)(w[3]);
+    t_float *in2 = (t_float *)(w[4]);
+    t_float *in3 = (t_float *)(w[5]);
+    t_float *out = (t_float *)(w[6]);
+    t_float last = x->x_last;
     while (nblock--)
     {
-    	float in = *in1++;
-    	float lo = *in2++;
-    	float hi = *in3++;
-        lastout = (in > lo && (in >= hi || lastout));
+        float in = *in1++;
+        float lo = *in2++;
+        float hi = *in3++;
+        last = (in > lo && (in >= hi || last));
         in1++;
-        *out++ = lastout;
+        *out++ = last;
     }
-    x->x_lastout = lastout;
-    return (w + 6);
+    x->x_last = last;
+    return (w + 7);
 }
 
 static void thresh_dsp(t_thresh *x, t_signal **sp)
 {
-    dsp_add(thresh_perform, 5, sp[0]->s_n,
-	    sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
+    dsp_add(thresh_perform, 6, x, sp[0]->s_n,
+            sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
 }
 
 static void *thresh_new(t_symbol *s, int ac, t_atom *av)
 {
     t_thresh *x = (t_thresh *)pd_new(thresh_class);
-    x->x_lastout = 0;
     sic_inlet((t_sic *)x, 1, THRESH_DEFLO, 0, ac, av);
     sic_inlet((t_sic *)x, 2, THRESH_DEFHI, 1, ac, av);
     outlet_new((t_object *)x, &s_signal);
+    x->x_last = 0;
     return (x);
 }
 
 void thresh_tilde_setup(void)
 {
     thresh_class = class_new(gensym("thresh~"),
-			   (t_newmethod)thresh_new, 0,
-			   sizeof(t_thresh), 0, A_GIMME, 0);
-    class_addcreator((t_newmethod)thresh_new, gensym("thresh~"), A_GIMME, 0);
-    class_addcreator((t_newmethod)thresh_new, gensym("cyclone/thresh~"), A_GIMME, 0);
+                                (t_newmethod)thresh_new, 0,
+                                sizeof(t_thresh), 0, A_GIMME, 0);
     sic_setup(thresh_class, thresh_dsp, SIC_FLOATTOSIGNAL);
 }
