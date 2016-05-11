514855b09ea98efd6f1fd7c31e63622bb76b9b59
porres <porres@gmail.com>
2016-05-11 12:00:18 -0300

    updating overdrive

--- classes/binaries/signal/overdrive.c
+++ classes/binaries/signal/overdrive.c
@@ -4,7 +4,6 @@
 
 #include <math.h>
 #include "m_pd.h"
-#include "sickle/sic.h"
 
 /* FIXME this is unnecessary in vc > 6.0 and darwin? */
 #if defined(_WIN32) || defined(__APPLE__)
@@ -13,20 +12,15 @@
 
 typedef struct _overdrive
 {
-    t_sic  x_sic;
+    t_object x_obj;
+    t_inlet  *x_inlet;
     float  x_drivefactor;
 } t_overdrive;
 
 static t_class *overdrive_class;
 
-/* CHECKED this is redundant (a design flaw), LATER fitter-optionally use
-   float-to-signal conversion. */
+// a design flaw, redundanct and undocumented in Max
 static void overdrive_float(t_overdrive *x, t_float f)
-{
-    x->x_drivefactor = f;
-}
-
-static void overdrive_ft1(t_overdrive *x, t_floatarg f)
 {
     x->x_drivefactor = f;
 }
@@ -34,13 +28,16 @@ static void overdrive_ft1(t_overdrive *x, t_floatarg f)
 /* CHECKED negative parameter values may cause output to go out of bounds */
 static t_int *overdrive_perform(t_int *w)
 {
-    float df = *(t_float *)(w[1]);
-    int nblock = (int)(w[2]);
-    t_float *in = (t_float *)(w[3]);
-    t_float *out = (t_float *)(w[4]);
+    t_overdrive *x = (t_overdrive *)(w[1]);
+    int nblock = (t_int)(w[2]);
+    t_float *in1 = (t_float *)(w[3]);
+    t_float *in2 = (t_float *)(w[4]);
+    t_float *out = (t_float *)(w[5]);
+    t_float df = x->x_drivefactor;
     while (nblock--)
     {
-	float f = *in++;
+	float f = *in1++;
+        df = *in2++;
 	if (f >= 1.)  /* CHECKED incompatible (garbage for sig~ 1.) */
 	    *out++ = 1.;  /* CHECKED constant for > 1. */
 	else if (f > 0.)
@@ -50,20 +47,28 @@ static t_int *overdrive_perform(t_int *w)
 	else
 	    *out++ = -1.;  /* CHECKED constant for < -1. */
     }
-    return (w + 5);
+    return (w + 6);
 }
 
 static void overdrive_dsp(t_overdrive *x, t_signal **sp)
 {
-    dsp_add(overdrive_perform, 4, &x->x_drivefactor,
-	    sp[0]->s_n, sp[0]->s_vec, sp[1]->s_vec);
+    dsp_add(overdrive_perform, 5, x, sp[0]->s_n,
+            sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec);
+}
+
+// FREE
+static void *overdrive_free(t_overdrive *x)
+{
+    inlet_free(x->x_inlet);
+    return (void *)x;
 }
 
 static void *overdrive_new(t_floatarg f)
 {
     t_overdrive *x = (t_overdrive *)pd_new(overdrive_class);
     x->x_drivefactor = f;
-    inlet_new((t_object *)x, (t_pd *)x, &s_float, gensym("ft1"));
+    x->x_inlet = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet, x->x_drivefactor);
     outlet_new((t_object *)x, &s_signal);
     return (x);
 }
@@ -71,9 +76,9 @@ static void *overdrive_new(t_floatarg f)
 void overdrive_tilde_setup(void)
 {
     overdrive_class = class_new(gensym("overdrive~"),
-				(t_newmethod)overdrive_new, 0,
-				sizeof(t_overdrive), 0, A_DEFFLOAT, 0);
-    sic_setup(overdrive_class, overdrive_dsp, overdrive_float);
-    class_addmethod(overdrive_class, (t_method)overdrive_ft1,
-		    gensym("ft1"), A_FLOAT, 0);
+				(t_newmethod)overdrive_new, (t_method)overdrive_free,
+				sizeof(t_overdrive), CLASS_DEFAULT, A_DEFFLOAT, 0);
+    class_addmethod(overdrive_class, nullfn, gensym("signal"), 0);
+    class_addmethod(overdrive_class, (t_method)overdrive_dsp, gensym("dsp"), A_CANT, 0);
+        class_addfloat(overdrive_class, (t_method)overdrive_float);
 }
