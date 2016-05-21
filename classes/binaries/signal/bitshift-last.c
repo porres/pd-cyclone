f2f3dd19221b9efd79fe027fe8703b03990cc4ef
porres <porres@gmail.com>
2016-05-21 17:31:11 -0300

    restoring original bitshift

--- classes/binaries/signal/bitshift-last.c
+++ classes/binaries/signal/bitshift-last.c
@@ -2,31 +2,27 @@
  * For information on usage and redistribution, and for a DISCLAIMER OF ALL
  * WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
 
-/* When bit-shifting 32-bit values, gcc (intel?) bashes the second operand
- modulo 32.  In msp x << 32 gives 0, x >> 32 gives a propagated sign bit
- (as expected).  Mimicking that is clumsy.  LATER consider making the calcs
- more generic (use long long values?) */
-
 #include "m_pd.h"
-#include "common/loud.h"
-#include "sickle/sic.h"
 
-#ifdef KRZYSZCZ
-//#define BITSHIFT_DEBUG
-#endif
+static t_class *bitshift_class;
 
-typedef struct _bitshift
-{
-    t_sic  x_sic;
+typedef struct _bitshift {
+    t_object x_obj;
+    t_outlet *x_outlet;
     int    x_convert1;
     int    x_lshift;
     int    x_rshift;
     int    x_lover;
 } t_bitshift;
 
-static t_class *bitshift_class;
+void *bitshift_new(t_floatarg f1, t_floatarg f2);
+static t_int * bitshift_perform(t_int *w);
+static void bitshift_dsp(t_bitshift *x, t_signal **sp);
+static void bitshift_mode(t_bitshift *x, t_floatarg f);
+static void bitshift_float(t_bitshift *x, t_float f);
 
-static t_int *bitshift_perform(t_int *w)
+
+static t_int * bitshift_perform(t_int *w)
 {
     t_bitshift *x = (t_bitshift *)(w[1]);
     int nblock = (int)(w[2]);
@@ -38,13 +34,11 @@ static t_int *bitshift_perform(t_int *w)
         unsigned int shift = x->x_lshift;
         if (x->x_convert1) while (nblock--)
         {
-            /* CHECKED */
             t_int i = ((t_int)*in++ << shift);
             *out++ = (t_float)i;
         }
         else while (nblock--)
         {
-            /* CHECKED */
             t_int i = (*(t_int *)(t_float *)in++ << shift);
             *out++ = *(t_float *)&i;
         }
@@ -54,14 +48,12 @@ static t_int *bitshift_perform(t_int *w)
         unsigned int shift = x->x_rshift;
         if (x->x_convert1) while (nblock--)
         {
-            /* CHECKME */
-            t_int i = ((t_int)*in++ >> shift);
+            t_int i = ((t_int)*in++ >> shift); /* CHECKME */
             *out++ = (t_float)i;
         }
         else while (nblock--)
         {
-            /* CHECKME */
-            t_int i = (*(t_int *)(t_float *)in++ >> shift);
+            t_int i = (*(t_int *)(t_float *)in++ >> shift);  /* CHECKME */
             *out++ = *(t_float *)&i;
         }
     }
@@ -80,7 +72,13 @@ static void bitshift_dsp(t_bitshift *x, t_signal **sp)
 static void bitshift_mode(t_bitshift *x, t_floatarg f)
 {
     int i = (int)f;
-    x->x_convert1 = (i > 0);  /* CHECKED */
+    x->x_convert1 = (i > 0);  /* CHECKME */
+}
+
+static void bitshift_float(t_bitshift *x, t_float f)
+{
+    int i = (int)f;
+    x->x_convert1 = (i > 0);  /* CHECKME */
 }
 
 static void bitshift_shift(t_bitshift *x, t_floatarg f)
@@ -91,10 +89,6 @@ static void bitshift_shift(t_bitshift *x, t_floatarg f)
     x->x_lover = 0;
     if (i > 0)
     {
-#ifdef BITSHIFT_DEBUG
-        loudbug_post("%.8x << %d == %.8x, %.8x << %d == %.8x",
-                     1, i, 1 << i, -1, i, -1 << i);
-#endif
         if (i < nbits)
             x->x_lshift = i;
         else
@@ -102,32 +96,25 @@ static void bitshift_shift(t_bitshift *x, t_floatarg f)
     }
     else if (i < 0)
     {
-#ifdef BITSHIFT_DEBUG
-        loudbug_post("%.8x >> %d == %.8x, %.8x >> %d == %.8x",
-                     0x7fffffff, -i, 0x7fffffff >> -i, -1, -i, -1 >> -i);
-#endif
         x->x_rshift = (i <= -nbits ? nbits - 1 : -i);
     }
 }
 
-static void *bitshift_new(t_floatarg f1, t_floatarg f2)
+void *bitshift_new(t_floatarg f1, t_floatarg f2)
 {
     t_bitshift *x = (t_bitshift *)pd_new(bitshift_class);
-    outlet_new((t_object *)x, &s_signal);
+    x->x_outlet = outlet_new(&x->x_obj, &s_signal);
     bitshift_shift(x, f1);
     bitshift_mode(x, f2);
     return (x);
 }
 
-void bitshift_tilde_setup(void)
-{
-    bitshift_class = class_new(gensym("bitshift~"),
-                               (t_newmethod)bitshift_new, 0,
-                               sizeof(t_bitshift), 0,
-                               A_DEFFLOAT, A_DEFFLOAT, 0);
-    sic_setup(bitshift_class, bitshift_dsp, SIC_FLOATTOSIGNAL);
-    class_addmethod(bitshift_class, (t_method)bitshift_mode,
-                    gensym("mode"), A_FLOAT, 0);
-    class_addmethod(bitshift_class, (t_method)bitshift_shift,
-                    gensym("shift"), A_FLOAT, 0);
+void bitshift_tilde_setup(void) { bitshift_class = class_new(gensym("bitshift~"),
+        (t_newmethod) bitshift_new, 0, sizeof (t_bitshift), CLASS_DEFAULT,
+        A_DEFFLOAT, A_DEFFLOAT, 0);
+    class_addfloat(bitshift_class, (t_method)bitshift_float);
+    class_addmethod(bitshift_class, nullfn, gensym("signal"), 0);
+    class_addmethod(bitshift_class, (t_method) bitshift_dsp, gensym("dsp"), 0);
+    class_addmethod(bitshift_class, (t_method)bitshift_mode, gensym("mode"), A_FLOAT, 0);
+    class_addmethod(bitshift_class, (t_method)bitshift_shift, gensym("shift"), A_FLOAT, 0);
 }
