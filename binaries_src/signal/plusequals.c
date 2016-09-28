4e16d0f681ea981e3bba6c5eda881c648c33988e
porres <porres@gmail.com>
2016-09-28 17:48:03 -0300

    pluequals no float in 2nd signal input

--- binaries_src/signal/plusequals.c
+++ binaries_src/signal/plusequals.c
@@ -61,6 +61,15 @@ static t_int *plusequals_perform_no_in(t_int *w)
     t_plusequals *x = (t_plusequals *)(w[1]);
     int nblock = (t_int)(w[2]);
     t_float *out = (t_float *)(w[5]);
+
+// MAGIC: poll float for error
+    t_float scalar = *x->x_signalscalar;
+    if (scalar != x->x_badfloat)
+    {
+        x->x_badfloat = scalar;
+        pd_error(x, "inlet: expected 'signal' but got 'float'");
+    }
+    
     while (nblock--)
     {
     *out++ = 0;
@@ -70,15 +79,12 @@ static t_int *plusequals_perform_no_in(t_int *w)
 
 static void plusequals_dsp(t_plusequals *x, t_signal **sp)
 {
-    if (forky_hasfeeders((t_object *)x, x->x_glist, 0, &s_signal))
-    {
-// MAGIC
-        x->x_hasfeeders = forky_hasfeeders((t_object *)x, x->x_glist, 1, &s_signal);
+    // MAGIC
+    x->x_hasfeeders = forky_hasfeeders((t_object *)x, x->x_glist, 1, &s_signal);
 
+    if (forky_hasfeeders((t_object *)x, x->x_glist, 0, &s_signal))
         dsp_add(plusequals_perform, 5, x, sp[0]->s_n,
 	    sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec);
-    }
-    
     else
         dsp_add(plusequals_perform_no_in, 5, x, sp[0]->s_n,
         sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec);
