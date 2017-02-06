c5b630e43a8fcb1057d45c5ce3671d078ccde0d8
porres <porres@gmail.com>
2017-02-05 22:59:28 -0200

    no progress with poke :/

--- cyclone_src/binaries/signal/poke.c
+++ cyclone_src/binaries/signal/poke.c
@@ -2,8 +2,6 @@
  * For information on usage and redistribution, and for a DISCLAIMER OF ALL
  * WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
 
-/* LATER: 'click' method */
-
 
 /*dearsic-ing and fragile-ing (plucked the necessary bits from it)
  
@@ -20,7 +18,7 @@
 #include "m_pd.h"
 #include "cybuf.h"
 
-#define POKE_MAXCHANNELS  64  /* LATER implement arsic resizing feature */
+#define POKE_MAXCHANNELS  64
 #define POKE_REDRAWMS 500 //redraw time in ms
 
 union inletunion
@@ -42,7 +40,7 @@ struct _inlet
     union inletunion i_un;
 };
 
-//end extra stuff for x->x_indexptr
+// end extra stuff for x->x_indexptr
 
 typedef struct _poke
 {
@@ -50,11 +48,10 @@ typedef struct _poke
     t_cybuf   *x_cybuf;
     t_sample  *x_indexptr;
     t_clock   *x_clock;
-    int         x_channum; //current channel number (1-indexed)
+    int        x_channum; //current channel number (1-indexed)
     double     x_clocklasttick;
     int        x_clockset;
-    double      x_redrawms; //time to redraw in ms
-
+    double     x_redrawms; //time to redraw in ms
     t_inlet   *x_idxlet;
 } t_poke;
 
@@ -73,16 +70,16 @@ static void poke_set(t_poke *x, t_symbol *s)
 }
 
 //redraw method/limiter
-static void poke_redraw_lim(t_poke *x){
+static void poke_redraw_lim(t_poke *x)
+{
     double redrawms = x->x_redrawms;
     double timesince = clock_gettimesince(x->x_clocklasttick);
     if (timesince > redrawms ) poke_tick(x);
     else if (!x->x_clockset)
-    {
-	clock_delay(x->x_clock, redrawms - timesince);
-	x->x_clockset = 1;
-    };
-
+        {
+        clock_delay(x->x_clock, redrawms - timesince);
+        x->x_clockset = 1;
+        };
 }
 
 static void poke_redraw_force(t_poke *x){
@@ -99,25 +96,24 @@ static void poke_redraw_force(t_poke *x){
    when dsp is on -- whether a signal is connected to the left inlet, or not
    (if not, current index is set to zero).  Incompatible (revisit LATER) */
 
-// not true, it should write when the dsp is on! (porres 2017)
+// Above is not true, it should write when the dsp is on! (porres 2017)
 
 static void poke_float(t_poke *x, t_float f)
-{
-
+    {
     t_cybuf *c = x->x_cybuf;
     t_word *vp = c->c_vectors[0];
-    //second arg is to allow error posting // ??? (porres)
-    cybuf_validate(c, 1);  /* LATER rethink (efficiency, and complaining) */
+    //second arg is to allow error posting
+    cybuf_validate(c, 1);  // LATER rethink (efficiency, and complaining)
     if (vp)
-    {
-	int ndx = (int)*x->x_indexptr;
-	if (ndx >= 0 && ndx < c->c_npts)
-	{
-	    vp[ndx].w_float = f;
+        {
+        int index = (int)*x->x_indexptr;
+        if (index >= 0 && index < c->c_npts)
+            {
+            vp[index].w_float = f;
             poke_redraw_lim(x);
-	};
+            };
+        }
     }
-}
 
 static void poke_ft2(t_poke *x, t_floatarg f)
 {
@@ -136,21 +132,20 @@ static t_int *poke_perform(t_int *w)
     t_poke *x = (t_poke *)(w[1]);
     t_cybuf *c = x->x_cybuf;
     int nblock = (int)(w[2]);
-    t_float *in1 = (t_float *)(w[3]);
-    t_float *in2 = (t_float *)(w[4]);
+    t_float *in1 = (t_float *)(w[3]); // value
+    t_float *in2 = (t_float *)(w[4]); // index
     t_word *vp = c->c_vectors[0];
-    if (vp && c->c_playable)
-    {
+    if (vp && c->c_playable) // ??? (porres)
+        {
         poke_redraw_lim(x);
-	int npts = c->c_npts;
-	while (nblock--)
-	{
-	    t_float f = *in1++;
-	    int ndx = (int)*in2++;
-	    if (ndx >= 0 && ndx < npts)
-		vp[ndx].w_float = f;
-	}
-    }
+        int npts = c->c_npts;
+        while (nblock--)
+            {
+            t_float value = *in1++; // value
+            int index = (int)*in2++; // index
+            if (index >= 0 && index < npts) vp[index].w_float = value;
+            }
+        }
     return (w + 5);
 }
 
@@ -171,29 +166,25 @@ static void *poke_new(t_symbol *s, t_floatarg f)
 {
     int ch = (f < 1) ? 1 : (f > CYBUF_MAXCHANS) ? CYBUF_MAXCHANS : (int)f;
 	t_poke *x = (t_poke  *)pd_new(poke_class);
-
     x->x_cybuf = cybuf_init((t_class *) x, s, 1, ch);
-    if (x)
-    {
+    if (x) // ??? (porres)
+        {
         x->x_channum = ch;
         x->x_redrawms = POKE_REDRAWMS; //default redraw rate
-	/* CHECKED: no float-to-signal conversion.
-	   Floats in 2nd inlet are ignored when dsp is on, but only if a signal
-	   is connected to this inlet.  Incompatible (revisit LATER). */
         
-        // not true, it should write when the dsp is on! (porres 2017)
-        
-	x->x_idxlet = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
+/* floats in 2nd inlet should be ignored when dsp is on
+   when a signal is connected (porres 2017) */
+        x->x_idxlet = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
 
-        //plucked from old unstable/fragile.c. found in pd's m_obj.c, basically plucks the float value
-        //from the signal without the float-to-signal conversion, I think... - DK
-        
-	x->x_indexptr = &(x->x_idxlet)->i_un.iu_floatsignalvalue;
-	inlet_new((t_object *)x, (t_pd *)x, &s_float, gensym("ft2"));
-	x->x_clock = clock_new(x, (t_method)poke_tick);
-	x->x_clocklasttick = clock_getlogicaltime();
-	x->x_clockset = 0;
-    }
+/* plucked from old unstable/fragile.c. found in pd's m_obj.c, basically plucks the 
+   float value from the signal without the float-to-signal conversion, I think... - DK */
+        x->x_indexptr = &(x->x_idxlet)->i_un.iu_floatsignalvalue;
+            
+        inlet_new((t_object *)x, (t_pd *)x, &s_float, gensym("ft2"));
+        x->x_clock = clock_new(x, (t_method)poke_tick);
+        x->x_clocklasttick = clock_getlogicaltime();
+        x->x_clockset = 0;
+        }
     return (x);
 }
 
