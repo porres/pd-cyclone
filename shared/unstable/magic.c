03e3d10e47edf919248237c14b296fe1d8f8abf8
porres <porres@gmail.com>
2017-04-01 23:02:32 -0300

    deleted about everything else from magic and it works :)

--- shared/unstable/magic.c
+++ shared/unstable/magic.c
@@ -1,54 +1,28 @@
-/* Copyright (c) 1997-2005 Miller Puckette, krzYszcz, and others.
- * For information on usage and redistribution, and for a DISCLAIMER OF ALL
- * WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
-
-/* Put here bits and pieces likely to break with any new Pd version. */
+// from old fragile (bits and pieces likely to break with any new Pd version.)
 
 #include <string.h>
 #include "m_pd.h"
 #include "unstable/magic.h"
 #include "unstable/pd_imp.h"
 
-
-int magic_class_count(void)
-{
-    return (pd_objectmaker->c_nmethod);
-}
-
-
-/* This structure is local to g_array.c.  We need it,
-   because there is no other way to get into array's graph. */
-struct _garray
-{
-    t_gobj x_gobj;
-    t_glist *x_glist;
-    /* ... */
-};
-
-t_glist *magic_garray_glist(void *arr)
-{
-    return (((struct _garray *)arr)->x_glist);
-}
-
-/* This is local to m_obj.c.
-   LATER export write access to o_connections field ('grab' class).
-   LATER encapsulate 'traverseoutlet' routines (not in the stable API yet). */
-struct _outlet
+struct _outlet // local to m_obj.c.
 {
     t_object *o_owner;
     struct _outlet *o_next;
     t_outconnect *o_connections;
     t_symbol *o_sym;
 };
+/* LATER export write access to o_connections field ('grab' class).
+LATER encapsulate 'traverseoutlet' routines (not in the stable API yet). */
 
-/* obj_starttraverseoutlet() replacement */
-t_outconnect *magic_outlet_connections(t_outlet *o)
-{
+
+t_outconnect *magic_outlet_connections(t_outlet *o) // obj_starttraverseoutlet() replacement
+    {
     return (o ? o->o_connections : 0);
-}
+    }
 
 t_outconnect *magic_outlet_nextconnection(t_outconnect *last, t_object **destp, int *innop)
-{
+    {
     t_inlet *dummy;
     return (obj_nexttraverseoutlet(last, destp, &dummy, innop));
-}
+    }
--- shared/unstable/magic.h
+++ shared/unstable/magic.h
@@ -1,24 +1,5 @@
-/* Copyright (c) 2005 krzYszcz and others.
- * For information on usage and redistribution, and for a DISCLAIMER OF ALL
- * WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
+// from old fragile (bits and pieces likely to break with any new Pd version.)
 
-#ifndef __MAGIC_H__
-#define __MAGIC_H__
-
-t_symbol *magic_class_getexterndir(t_class *c);
-int magic_class_count(void);
-int magic_class_getnames(t_atom *av, int maxnames);
-void magic_class_raise(t_symbol *cname, t_newmethod thiscall);
-t_pd *magic_class_mutate(t_symbol *cname, t_newmethod thiscall,
-			   int ac, t_atom *av);
-t_newmethod magic_class_getalien(t_symbol *cname, t_newmethod thiscall,
-				   t_atomtype **argtypesp);
-t_pd *magic_class_createobject(t_symbol *cname, t_newmethod callthis,
-				 t_atomtype *argtypes, int ac, t_atom *av);
-void magic_class_printnames(char *msg, int firstndx, int lastndx);
-t_glist *magic_garray_glist(void *arr);
 t_outconnect *magic_outlet_connections(t_outlet *o);
 t_outconnect *magic_outlet_nextconnection(t_outconnect *last, t_object **destp, int *innop);
-t_sample *magic_inlet_signalscalar(t_inlet *i);
 
-#endif
