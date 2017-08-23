795a024d3d99b1249dab96f31d06a0da94d18f2d
porres <porres@gmail.com>
2017-08-23 00:57:30 -0300

    cyclone

--- cyclone_src/binaries/sub_lib_cyclone.c
+++ cyclone_src/binaries/sub_lib_cyclone.c
@@ -657,7 +657,7 @@ void cyclone_setup(void)
     if(!printed)
     {
         post("------------------------------------------------------------------");
-        post("Cyclone 0.3 beta 3; Released: July 3rd, 2017");
+        post("Cyclone 0.3 beta 3; Released: xxxx, 2017");
         post("This is a sub library containing the objects:");
         post("[!-], [!-~], [!/], [!/~], [!=~], [%%~], [+=~], [<=~], [<~], [==~], [>=~] and [>~]");
         post("------------------------------------------------------------------");
