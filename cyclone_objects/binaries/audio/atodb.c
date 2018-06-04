514fee9a2eb5230e92d8b94e4ce474d7c91b2e5e
porres <porres@gmail.com>
2018-06-04 01:18:22 -0300

    oops

--- cyclone_objects/binaries/audio/atodb.c
+++ cyclone_objects/binaries/audio/atodb.c
@@ -14,32 +14,31 @@
 
 static t_class *atodb_class;
 
-typedef struct _atodb {
+typedef struct _atodb{
   t_object x_obj;
   t_inlet *x_inlet;
   t_outlet *x_outlet;
-} t_atodb;
+}t_atodb;
 
 static t_int * atodb_perform(t_int *w){
-   t_atodb *x = (t_atodb *)(w[1]);
-  int n = (int)(w[2]);
-  t_float *in = (t_float *)(w[3]);
-  t_float *out = (t_float *)(w[4]);
-  while(n--)
-      t_float output = (20*log10(*in++));
-    if(output < -999)
-        output = -999;
-    *out++ = output;
-  return (w + 5);
+    t_atodb *x = (t_atodb *)(w[1]);
+    int n = (int)(w[2]);
+    t_float *in = (t_float *)(w[3]);
+    t_float *out = (t_float *)(w[4]);
+    while(n--){
+        t_float output = (20*log10(*in++));
+        if(output < -999)
+            output = -999;
+        *out++ = output;
+    }
+    return(w+5);
 }
 
-static void atodb_dsp(t_atodb *x, t_signal **sp)
-{
+static void atodb_dsp(t_atodb *x, t_signal **sp){
   dsp_add(atodb_perform, 4, x, sp[0]->s_n, sp[0]->s_vec, sp[1]->s_vec);
 }
 
-static void *atodb_new(void)
-{
+static void *atodb_new(void){
   t_atodb *x = (t_atodb *)pd_new(atodb_class);
   x->x_outlet = outlet_new(&x->x_obj, &s_signal);
   return (void *)x;
@@ -47,7 +46,7 @@ static void *atodb_new(void)
 
 void atodb_tilde_setup(void) {
   atodb_class = class_new(gensym("atodb~"),
-    (t_newmethod) atodb_new, 0, sizeof (t_atodb), CLASS_DEFAULT, 0);
+       (t_newmethod) atodb_new, 0, sizeof (t_atodb), CLASS_DEFAULT, 0);
   class_addmethod(atodb_class, nullfn, gensym("signal"), 0);
   class_addmethod(atodb_class, (t_method) atodb_dsp, gensym("dsp"), A_CANT, 0);
 }
