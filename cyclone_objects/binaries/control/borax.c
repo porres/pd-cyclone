46905a8b14cea7f76349a70cc47cf33354ec15d5
porres <porres@gmail.com>
2018-06-17 01:29:20 -0300

    fix borax

--- cyclone_objects/binaries/control/borax.c
+++ cyclone_objects/binaries/control/borax.c
@@ -11,13 +11,15 @@
 
 #define BORAX_MAXVOICES  128  /* CHECKME */
 
-typedef struct _Borax_voice{
+typedef struct _Borax_voice
+{
     int     v_index;  /* free iff zero */
     double  v_onset;
     int     v_nonsets;
 } t_Borax_voice;
 
-typedef struct _Borax{
+typedef struct _Borax
+{
     t_object        x_ob;
     int             x_vel;  /* CHECKME t_float controlled with floatinlet
 			       (CHECKME the same in flush) */
@@ -41,7 +43,9 @@ typedef struct _Borax{
 
 static t_class *Borax_class;
 
-static void Borax_delta(t_Borax *x){ /* CHECKME first note */
+static void Borax_delta(t_Borax *x)
+{
+    /* CHECKME first note */
     float dtime = clock_gettimesince(x->x_onset);  /* CHECKME */
     outlet_float(x->x_dtimeout, dtime);
     outlet_float(x->x_ndtimesout, ++x->x_ndtimes);  /* CHECKME */
@@ -55,18 +59,20 @@ static void Borax_durout(t_Borax *x, int pitch)
 }
 
 static void Borax_float(t_Borax *x, t_float f){
-    if((int)f == f){
-        int pitch;
+    int pitch = (int)f;
+    if((f - (t_float)pitch) == 0.){
         int index;
-        if (pitch < 0 || pitch >= BORAX_MAXVOICES) // CHECKME pitch range, complaints
+        if (pitch < 0 || pitch >= BORAX_MAXVOICES){
+            /* CHECKME pitch range, complaints */
             return;
+        }
         index = x->x_voices[pitch].v_index;
         if (x->x_vel){
             if (index)
                 return;  /* CHECKME */
             x->x_indices[index = x->x_minindex] = 1;
             while (x->x_indices[++x->x_minindex]);
-                index++;  /* CHECKME one-based? */
+            index++;  /* CHECKME one-based? */
             Borax_delta(x);
             x->x_onset = clock_getlogicaltime();  /* CHECKME (in delta?) */
             x->x_voices[pitch].v_index = index;
@@ -75,12 +81,12 @@ static void Borax_float(t_Borax *x, t_float f){
             x->x_nvoices++;
         }
         else{
-            if(!index)
+            if (!index)
                 return;  /* CHECKME */
             index--;
             x->x_indices[index] = 0;
-            if(index < x->x_minindex) x->x_minindex = index;
-                index++;
+            if (index < x->x_minindex) x->x_minindex = index;
+            index++;
             Borax_durout(x, pitch);
             x->x_voices[pitch].v_index = 0;
             x->x_nvoices--;
@@ -91,16 +97,15 @@ static void Borax_float(t_Borax *x, t_float f){
         outlet_float(x->x_voiceout, index);
         outlet_float(((t_object *)x)->ob_outlet, x->x_voices[pitch].v_nonsets);
     }
-    else{
-        pd_error(x, "[borax]: doesn't understand \"noninteger float\"");
-    }
 }
 
-static void Borax_ft1(t_Borax *x, t_floatarg f){
+static void Borax_ft1(t_Borax *x, t_floatarg f)
+{
     x->x_vel = (int)f;  /* CHECKME */
 }
 
-static void Borax_reset(t_Borax *x){
+static void Borax_reset(t_Borax *x)
+{
     x->x_vel = 0;
     x->x_onset = clock_getlogicaltime();
     x->x_nonsets = x->x_ndurs = x->x_ndtimes = 0;
@@ -115,7 +120,8 @@ static void Borax_bang2(t_Borax *x)
     int pitch;
     for (pitch = 0; pitch < BORAX_MAXVOICES; pitch++)
     {
-	if (x->x_voices[pitch].v_index){
+	if (x->x_voices[pitch].v_index)
+	{
 	    /* CHECKME counters, etc. */
 	    Borax_durout(x, pitch);
 	    outlet_float(x->x_velout, 0);
@@ -131,7 +137,8 @@ static void Borax_bang2(t_Borax *x)
 
 /* CHECKME flush in a destructor */
 
-static void *Borax_new(void){
+static void *Borax_new(void)
+{
     t_Borax *x = (t_Borax *)pd_new(Borax_class);
     inlet_new((t_object *)x, (t_pd *)x, &s_float, gensym("ft1"));
     inlet_new((t_object *)x, (t_pd *)x, &s_bang, gensym("bang2"));
@@ -148,18 +155,23 @@ static void *Borax_new(void){
     return (x);
 }
 
-void borax_setup(void){
+void borax_setup(void)
+{
     Borax_class = class_new(gensym("borax"),
         (t_newmethod)Borax_new, 0, sizeof(t_Borax), 0, 0);
     class_addcreator((t_newmethod)Borax_new, gensym("Borax"), 0, 0);
     class_addcreator((t_newmethod)Borax_new, gensym("cyclone/Borax"), 0, 0);
     class_addfloat(Borax_class, Borax_float);
     /* CHECKME list unfolding */
-    class_addmethod(Borax_class, (t_method)Borax_ft1, gensym("ft1"), A_FLOAT, 0);
-    class_addmethod(Borax_class, (t_method)Borax_bang2, gensym("bang2"), 0);
-    class_addmethod(Borax_class, (t_method)Borax_delta, gensym("delta"), 0);
+    class_addmethod(Borax_class, (t_method)Borax_ft1,
+		    gensym("ft1"), A_FLOAT, 0);
+    class_addmethod(Borax_class, (t_method)Borax_bang2,
+		    gensym("bang2"), 0);
+    class_addmethod(Borax_class, (t_method)Borax_delta,
+		    gensym("delta"), 0);
 }
 
-void Borax_setup(void){
+void Borax_setup(void)
+{
     borax_setup();
 }
