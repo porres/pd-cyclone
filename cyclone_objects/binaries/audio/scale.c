f5966cdb66354c598b42f79925ba8a3e7d34d301
porres <porres@gmail.com>
2018-06-27 22:00:23 -0300

    fix scale~

--- cyclone_objects/binaries/audio/scale.c
+++ cyclone_objects/binaries/audio/scale.c
@@ -1,247 +1,187 @@
-/************
- Copyright (c) 2016 Marco Matteo Markidis
- mm.markidis@gmail.com
- 
- For information on usage and redistribution, and for a DISCLAIMER OF ALL
- WARRANTIES, see the file, "LICENSE.txt," in this distribution.
-
- Made while listening:
- Hiatus Kaiyote -- Choose Your Weapon
-*************/
-
-// Porres 2017 made some changes to fix some issues
-
-#include <string.h>
+// Porres 2016
 
 #include "m_pd.h"
 #include <math.h>
+#include <string.h>
 
-
-static t_class *scale_class;
+#define SCALE_MININ  0.
+#define SCALE_MAXIN  127.
+#define SCALE_MINOUT  0.
+#define SCALE_MAXOUT  1.
+#define SCALE_EXPO  1.
 
 typedef struct _scale
 {
-  t_object obj;
-  t_float in; // input
-  t_float f_in; // float input
-  t_outlet *float_outlet;
-  t_float minin;
-  t_float maxin;
-  t_float minout;
-  t_float maxout;
-  t_float expo;
-  t_float exp_in;
-  t_atom *output_list; /* for list output */
-  t_int a_bytes;
-  t_int flag;
-  t_int ac;
+    t_object x_obj;
+    t_inlet  *x_inlet_1;
+    t_inlet  *x_inlet_2;
+    t_inlet  *x_inlet_3;
+    t_inlet  *x_inlet_4;
+    t_inlet  *x_inlet_5;
+    t_int    x_classic;
 } t_scale;
 
-static void *scale_new(t_symbol *s, int argc, t_atom *argv);
-static void scale_ft(t_scale *x, t_floatarg f);
-static void scale_bang(t_scale *x);
-static void scale_list(t_scale *x, t_symbol *s, int argc, t_atom *argv);
-static void scale_free(t_scale *x);
-static void scale_classic(t_scale *x, t_floatarg f);
-
-static t_float scaling(t_scale *x, t_float f);
-static t_float exp_scaling(t_scale *x, t_float f);
-static t_float clas_scaling(t_scale *x, t_float f);
-static t_float (*ptrtoscaling)(t_scale *x,t_float f);
-static void check(t_scale *x);
-
-static void *scale_new(t_symbol *s, int argc, t_atom *argv)
-{
-  t_scale *x = (t_scale *)pd_new(scale_class);
-  x->float_outlet = outlet_new(&x->obj, 0);
-  floatinlet_new(&x->obj,&x->minin);
-  floatinlet_new(&x->obj,&x->maxin);
-  floatinlet_new(&x->obj,&x->minout);
-  floatinlet_new(&x->obj,&x->maxout);
-  floatinlet_new(&x->obj,&x->exp_in);
-  x->minin = 0;
-  x->maxin = 127;
-  x->minout = 0;
-  x->maxout = 1;
-  x->flag = 0;
-  x->exp_in = 1.f;
-  t_int numargs = 0;
-
-  while(argc>0) {
-    t_symbol *firstarg = atom_getsymbolarg(0,argc,argv);
-    if(firstarg==&s_){
-      switch(numargs) {
-      case 0:
-	x->minin = atom_getfloatarg(0,argc,argv);
-	numargs++;
-	argc--;
-	argv++;
-	break;
-      case 1:
-	x->maxin = atom_getfloatarg(0,argc,argv);
-	numargs++;
-	argc--;
-	argv++;
-	break;
-      case 2:
-	x->minout = atom_getfloatarg(0,argc,argv);
-	numargs++;
-	argc--;
-	argv++;
-	break;
-      case 3:
-	x->maxout = atom_getfloatarg(0,argc,argv);
-	numargs++;
-	argc--;
-	argv++;
-	break;
-      case 4:
-	x->exp_in = atom_getfloatarg(0,argc,argv);
-	numargs++;
-	argc--;
-	argv++;
-	break;
-      default:
-	argc--;
-	argv++;
-      }
-    }
-    else {
-      t_int isclassic = strcmp(firstarg->s_name,"@classic") == 0;
-      if(isclassic && argc>=1) {
-	t_symbol *arg = atom_getsymbolarg(1,argc,argv);
-	if(arg == &s_) {
-	  t_int dummy = atom_getintarg(1, argc, argv);
-	  if(dummy == 1)
-	    x->flag = 1;
-	  argc-=2;
-	  argv+=2;
-	}
-      }
-      else {
-	argc = 0;
-	pd_error(x,"scale: improper args");
-      }
-    }
-  }
-
-  if(x->flag == 0)
-    {
-    if(x->exp_in < 0.f) x->exp_in = 0;
-    }
-  else
-    {
-    if(x->exp_in < 1.f) x->exp_in = 1;
-    }
-  
-  x->ac = 1;
-  x->a_bytes = x->ac*sizeof(t_atom);
-  x->output_list = (t_atom *)getbytes(x->a_bytes);
-  if(x->output_list==NULL) {
-    pd_error(x,"scale: memory allocation failure");
-    return NULL;
-  }
-  
-  return (x);
-}
-
-void scale_setup(void)
-{
-  t_class *c;
-  scale_class = class_new(gensym("scale"), (t_newmethod)scale_new,
-			  (t_method)scale_free,sizeof(t_scale),0,A_GIMME,0);
-  c = scale_class;
-  class_addfloat(c,(t_method)scale_ft);
-  class_addbang(c,(t_method)scale_bang);
-  class_addlist(c,(t_method)scale_list);
-  class_addmethod(c,(t_method)scale_classic,gensym("classic"),A_DEFFLOAT,0);
-}
+static t_class *scale_class;
 
 
 static void scale_classic(t_scale *x, t_floatarg f)
 {
-    x->flag = f;
-    check(x);
+    x->x_classic = (int)f;
 }
 
-
-static void scale_ft(t_scale *x, t_floatarg f)
+static t_int *scale_perform(t_int *w)
 {
-  x->in = x->f_in = f;
-  check(x);
-  t_float temp = ptrtoscaling(x, f);
-  outlet_float(x->float_outlet, temp);
-  return;
-}
-
-static t_float scaling(t_scale *x, t_float f)
-{
-  f = (x->maxout - x->minout)*(f-x->minin)/(x->maxin-x->minin) + x->minout;
-  return f;
-}
-
-static t_float exp_scaling(t_scale *x, t_float f)
-{
-  f = ((f-x->minin)/(x->maxin-x->minin) == 0) 
-    ? x->minout : (((f-x->minin)/(x->maxin-x->minin)) > 0) 
-    ? (x->minout + (x->maxout-x->minout) * pow((f-x->minin)/(x->maxin-x->minin),x->expo)) 
-    : ( x->minout + (x->maxout-x->minout) * -(pow(((-f+x->minin)/(x->maxin-x->minin)),x->expo)));
-  return f;
-}
-
-static t_float clas_scaling(t_scale *x, t_float f)
-{
-  f = (x->maxout-x->minout >= 0) ?
-    (x->minout + (x->maxout-x->minout) * ( (x->maxout - x->minout) * exp(-1*(x->maxin-x->minin)*log(x->expo)) * exp(f*log(x->expo)) )) :
-    (-1) * ( x->minout + (x->maxout-x->minout) * ( (x->maxout - x->minout) * exp(-1*(x->maxin-x->minin)*log(x->expo)) * exp(f*log(x->expo))));
-  return f;
-}
-
-static void scale_bang(t_scale *x)
-{
-  scale_ft(x, x->f_in);
-  return;
-}
-
-static void scale_list(t_scale *x, t_symbol *s, int argc, t_atom *argv)
-{
-  int i = 0;
-  int old_a = x->a_bytes;
-  x->ac = argc;
-  x->a_bytes = argc*sizeof(t_atom);
-  x->output_list = (t_atom *)t_resizebytes(x->output_list,old_a,x->a_bytes);
-  check(x);
-  x->in = atom_getfloatarg(0,argc,argv);
-  for(i=0;i<argc;i++)
-    SETFLOAT(x->output_list + i, ptrtoscaling(x, atom_getfloatarg(i, argc, argv)));
-  outlet_list(x->float_outlet, 0, argc, x->output_list);
-  return;
-}
-
-static void check(t_scale *x)
-{
-    if(x->flag == 1)
+    t_scale *x = (t_scale *)(w[1]);
+    int nblock = (int)(w[2]);
+    t_float *in1 = (t_float *)(w[3]);
+    t_float *in2 = (t_float *)(w[4]);
+    t_float *in3 = (t_float *)(w[5]);
+    t_float *in4 = (t_float *)(w[6]);
+    t_float *in5 = (t_float *)(w[7]);
+    t_float *in6 = (t_float *)(w[8]);
+    t_float *out = (t_float *)(w[9]);
+    t_int classic_flag = x->x_classic;
+    while (nblock--)
     {
-        x->expo = x->exp_in < 1. ? 1. : x->exp_in;
+    float in = *in1++;
+    float il = *in2++; // Input LOW
+    float ih = *in3++; // Input HIGH
+    float ol = *in4++; // Output LOW
+    float oh = *in5++; // Output HIGH
+    float p = *in6++; // power (exponential) factor
+    float output;
+    if (classic_flag != 0)
+        {p = p <= 1 ? 1 : p;
+        if (p == 1) output = ((in - il) / (ih - il) == 0) ? ol
+            : (((in - il) / (ih - il)) > 0) ? (ol + (oh - ol) * pow((in - il) / (ih - il), p))
+            : (ol + (oh - ol) * -(pow(((-in + il) / (ih - il)), p)));
+        else
+            {output = ol + (oh - ol) * ((oh - ol) * exp((il - ih) * log(p)) * exp(in * log(p)));
+            if (oh - ol <= 0) output = output * -1;
+            
+            }
+        }
+        else {
+        p = p <= 0 ? 0 : p;
+        output = ((in - il) / (ih - il) == 0) ? ol :
+        (((in - il) / (ih - il)) > 0) ?
+        (ol + (oh - ol) * pow((in - il) / (ih - il), p)) :
+        (ol + (oh - ol) * -(pow(((-in + il) / (ih - il)), p)));
+        }
+    *out++ = output;
     }
-    else
-    {
-        x->expo = x->exp_in < 0. ? 0. : x->exp_in;
-    }
-  switch (x->flag) {
-  case 0:
-    ptrtoscaling = exp_scaling;
-    break;
-  default:
-    ptrtoscaling = clas_scaling;
-    break;
-  }
-  if(x->expo == 1)
-    ptrtoscaling = scaling;
-  return;
+    return (w + 10);
 }
 
-static void scale_free(t_scale *x)
+static void scale_dsp(t_scale *x, t_signal **sp)
 {
-  t_freebytes(x->output_list,x->a_bytes);
+    dsp_add(scale_perform, 9, x, sp[0]->s_n, sp[0]->s_vec, sp[1]->s_vec,
+            sp[2]->s_vec, sp[3]->s_vec, sp[4]->s_vec, sp[5]->s_vec, sp[6]->s_vec);
+}
+
+static void *scale_free(t_scale *x)
+{
+		inlet_free(x->x_inlet_1);
+        inlet_free(x->x_inlet_2);
+        inlet_free(x->x_inlet_3);
+        inlet_free(x->x_inlet_4);
+        inlet_free(x->x_inlet_5);
+        return (void *)x;
+}
+
+static void *scale_new(t_symbol *s, int argc, t_atom *argv)
+{
+    t_scale *x = (t_scale *)pd_new(scale_class);
+    t_float min_in, max_in, min_out, max_out, exponential;
+    min_in = SCALE_MININ;
+    max_in = SCALE_MAXIN;
+    min_out = SCALE_MINOUT;
+    max_out = SCALE_MAXOUT;
+    exponential = SCALE_EXPO;
+    t_int classic_exp;
+    classic_exp = 0;
+    
+	int argnum = 0;
+	while(argc > 0){
+		if(argv -> a_type == A_FLOAT)
+        {
+			t_float argval = atom_getfloatarg(0,argc,argv);
+				switch(argnum){
+					case 0:
+						min_in = argval;
+						break;
+					case 1:
+						max_in = argval;
+						break;
+                    case 2:
+                        min_out = argval;
+                        break;
+                    case 3:
+                        max_out = argval;
+                        break;
+                    case 4:
+                        exponential = argval;
+                        break;
+					default:
+						break;
+				};
+				argc--;
+				argv++;
+				argnum++;
+        }
+        
+// attribute
+        else if(argv -> a_type == A_SYMBOL){
+            t_symbol *curarg = atom_getsymbolarg(0, argc, argv);
+            if(strcmp(curarg->s_name, "@classic")==0){
+                if(argc >= 2){
+                    t_float argval = atom_getfloatarg(1, argc, argv);
+                    classic_exp = (int)argval;
+                    argc-=2;
+                    argv+=2;
+                }
+                else{
+                    goto errstate;
+                };
+            }
+            else{
+                goto errstate;
+            };
+            
+        }
+        else{
+            goto errstate;
+        };
+    };
+    
+	x->x_inlet_1 = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+	pd_float((t_pd *)x->x_inlet_1, min_in);
+    x->x_inlet_2 = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet_2, max_in);
+    x->x_inlet_3 = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet_3, min_out);
+    x->x_inlet_4 = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet_4, max_out);
+    x->x_inlet_5 = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet_5, exponential);
+
+    outlet_new((t_object *)x, &s_signal);
+    
+    x->x_classic = classic_exp;
+    
+    return (x);
+	errstate:
+		pd_error(x, "scale~: improper args");
+		return NULL;
+}
+
+void scale_tilde_setup(void)
+{
+    scale_class = class_new(gensym("scale~"),
+				(t_newmethod)scale_new,
+                (t_method)scale_free,
+				sizeof(t_scale), 0, A_GIMME, 0);
+    class_addmethod(scale_class, nullfn, gensym("signal"), 0);
+    class_addmethod(scale_class, (t_method)scale_dsp, gensym("dsp"), A_CANT, 0);
+    class_addmethod(scale_class, (t_method) scale_classic, gensym("classic"), A_DEFFLOAT, 0);
 }
