c9c2ae21c84eda538ad095851e12a678ad342198
porres <porres@gmail.com>
2022-06-07 23:00:09 -0300

    Update cyclone_lib.c

--- cyclone_objects/binaries/cyclone_lib.c
+++ cyclone_objects/binaries/cyclone_lib.c
@@ -641,23 +641,22 @@ void print_cyclone(t_cyclone *x){
     int major = 0, minor = 0, bugfix = 0;
     sys_getversion(&major, &minor, &bugfix);
     post("");
-    post("-----------------------------------------------------------------------------");
+    post("--------------------------------------------------------------------");
     post(":: Cyclone %d.%d-%d; Released june 8th 2022", cyclone_major, cyclone_minor, cyclone_bugfix);
     post(":: License: BSD-3-Clause (aka Revised BSD License)");
     post(":: Copyright © 2003-2021 - Krzysztof Czaja, Hans-Christoph Steiner,");
-    post(":: Fred Jan Kraan, Alexandre Porres, Derek Kwan, Matt Barber and others.");
-    post(":: ---------------------------------------------------------------------------");
+    post(":: Fred Jan Kraan, Alexandre Porres, Derek Kwan, Matt Barber\n\:: and others.");
+    post(":: -----------------------------------------------------------------");
     if((major > min_major)
        || (major == min_major && minor > min_minor)
        || (major == min_major && minor == min_minor && bugfix >= min_bugfix))
-        post(":: Cyclone %d.%d-%d needs at least Pd %d.%d-%d\
+        post(":: Cyclone %d.%d-%d needs at least Pd %d.%d-%d\n\
              (you have %d.%d-%d, you're good!)",
              cyclone_major, cyclone_minor, cyclone_bugfix,
              min_major, min_minor, min_bugfix,
              major, minor, bugfix);
     else
-        pd_error(x, ":: Cyclone %d.%d-%d needs at least Pd %d.%d-%d\
-                 (you have %d.%d-%d, please upgrade!)",
+        pd_error(x, ":: Cyclone %d.%d-%d needs at least Pd %d.%d-%d\n\:: (you have %d.%d-%d, please upgrade!)",
             cyclone_major, cyclone_minor, cyclone_bugfix,
             min_major, min_minor, min_bugfix,
             major, minor, bugfix);
@@ -667,9 +666,8 @@ void print_cyclone(t_cyclone *x){
     post(":: [==~], [>=~] and [>~]");
     post("::   - B) Added %s", cyclone_dir);
     post(":: to Pd's path so the other objects can be loaded too");
-    post(":: but use [declare -path cyclone] to guarantee search priority\
-         in the patch");
-    post("-----------------------------------------------------------------------------");
+    post(":: but use [declare -path cyclone] to guarantee search priority\n\:: in the patch");
+    post("--------------------------------------------------------------------");
     post("");
 }
 
