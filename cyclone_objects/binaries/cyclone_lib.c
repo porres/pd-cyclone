b6e9b2217c5eaec7f06cce2729508cceb6c6ea4e
porres <porres@gmail.com>
2024-06-20 16:28:30 -0300

    set release date

--- cyclone_objects/binaries/cyclone_lib.c
+++ cyclone_objects/binaries/cyclone_lib.c
@@ -641,7 +641,7 @@ void print_cyclone(t_cyclone *x){
     sys_getversion(&major, &minor, &bugfix);
     post("");
     post("--------------------------------------------------------------------");
-    post(":: Cyclone %d.%d-%d; Released June 12th 2024", cyclone_major, cyclone_minor, cyclone_bugfix);
+    post(":: Cyclone %d.%d-%d; Released June 20th 2024", cyclone_major, cyclone_minor, cyclone_bugfix);
     post(":: License: BSD-3-Clause (aka Revised BSD License)");
     post(":: Copyright © 2003-2024 - Krzysztof Czaja, Hans-Christoph Steiner,");
     post(":: Fred Jan Kraan, Alexandre Porres, Derek Kwan, Matt Barber\n\:: and others.");
