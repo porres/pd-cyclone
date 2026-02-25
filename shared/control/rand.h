54570eb40c9ed74f1dbd3f97cd89773cbe2cd742
porres <porres@gmail.com>
2026-02-24 23:26:59 -0300

    fix [drunk] random
    - closes https://github.com/porres/pd-cyclone/issues/685

--- shared/control/rand.h
+++ shared/control/rand.h
@@ -5,11 +5,8 @@
 #ifndef __RAND_H__
 #define __RAND_H__
 
-#define RAND_DEBUG
-
 void rand_seed(unsigned int *statep, unsigned int seed);
 int rand_int(unsigned int *statep, int range);
 float rand_unipolar(unsigned int *statep);
-float rand_bipolar(unsigned int *statep);
 
 #endif
