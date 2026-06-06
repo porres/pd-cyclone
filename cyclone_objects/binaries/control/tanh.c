7ed3b94c3f0e1702d035f268797ef951abb1fd79
porres <porres@gmail.com>
2026-06-06 19:55:15 -0300

    use [tanh] and [atan2~] from ELSE

--- cyclone_objects/binaries/control/tanh.c
+++ cyclone_objects/binaries/control/tanh.c
@@ -2,48 +2,47 @@
  * For information on usage and redistribution, and for a DISCLAIMER OF ALL
  * WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
 
+#include <m_pd.h>
+#include <else_alloca.h>
 #include <math.h>
-#include "m_pd.h"
+#include <stdlib.h>
 #include <common/api.h>
-
-#if defined(_WIN32) || defined(__APPLE__)
-/* cf pd/src/x_arithmetic.c */
-#define tanhf  tanh
-#endif
-
-typedef struct _tanh
-{
-    t_object  x_ob;
-    float     x_value;
-} t_tanh;
 
 static t_class *tanh_class;
 
-static void tanh_bang(t_tanh *x)
-{
-    outlet_float(((t_object *)x)->ob_outlet, x->x_value);
+typedef struct _tanh{
+    t_object    x_obj;
+    float       x_value;
+}t_tanh;
+
+static void tanh_list(t_tanh *x, t_symbol *s, int ac, t_atom *av){
+    s = NULL;
+    if(ac == 1)
+        outlet_float(x->x_obj.ob_outlet, x->x_value = tanhf(atom_getfloat(av)));
+    else if(ac > 1){
+        t_atom* at = ALLOCA(t_atom, ac);
+        
+        for(int i = 0; i < ac; i++)
+            SETFLOAT(at+i, tanhf(atom_getfloatarg(i, ac, av)));
+        outlet_list(x->x_obj.ob_outlet, &s_list, ac, at);
+        FREEA(at, t_atom, ac);
+    }
 }
 
-static void tanh_float(t_tanh *x, t_float f)
-{
-    /* CHECKME large values */
-    outlet_float(((t_object *)x)->ob_outlet, x->x_value = tanhf(f));
+static void tanh_bang(t_tanh *x){
+    outlet_float(x->x_obj.ob_outlet, x->x_value);
 }
 
-static void *tanh_new(t_floatarg f)
-{
+static void *tanh_new(t_floatarg f){
     t_tanh *x = (t_tanh *)pd_new(tanh_class);
-    /* CHECKME large values */
     x->x_value = tanhf(f);
-    outlet_new((t_object *)x, &s_float);
-    return (x);
+    outlet_new(&x->x_obj, 0);
+    return(x);
 }
 
-CYCLONE_OBJ_API void tanh_setup(void)
-{
-    tanh_class = class_new(gensym("tanh"),
-			   (t_newmethod)tanh_new, 0,
-			   sizeof(t_tanh), 0, A_DEFFLOAT, 0);
-    class_addbang(tanh_class, tanh_bang);
-    class_addfloat(tanh_class, tanh_float);
+CYCLONE_OBJ_API void tanh_setup(void){
+    tanh_class = class_new(gensym("tanh"), (t_newmethod)tanh_new, 0,
+        sizeof(t_tanh), 0, A_DEFFLOAT, 0);
+    class_addbang(tanh_class, (t_method)tanh_bang);
+    class_addlist(tanh_class, (t_method)tanh_list);
 }
